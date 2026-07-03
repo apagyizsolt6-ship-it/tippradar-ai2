d14a45bf5ee1f927e827638552ab60bcbc837fd4
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-03 20:37:31 +0200

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,1 +1,20 @@
-
+let matches = [
+  {
+    league: "NB I",
+    home: "FTC",
+    away: "Paks",
+    odds: 1.82
+  },
+  {
+    league: "Premier League",
+    home: "Liverpool",
+    away: "Arsenal",
+    odds: 1.74
+  },
+  {
+    league: "La Liga",
+    home: "Barcelona",
+    away: "Valencia",
+    odds: 1.67
+  }
+];
